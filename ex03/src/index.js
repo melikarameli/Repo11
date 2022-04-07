c88131917f816a75d9733947ecc9bc2b86136b26
melikarameli <melikahatunic2001@gmail.com>
2022-04-07 18:45:49 +0200

    Added ex04

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,17 +1,25 @@
-// Create a myPetsArray array here
-var myPetsArray = ["Dog", "Cat"];
-// End of myPetsArray array
-// Create a function below this line
-function myArrayFunction(myPets) {
-    var myNewPets = myPetsArray
-    myNewPets.push("Bird", "Fish");
-    var firstPet = myNewPets[0];
-    var lastPet = myNewPets[3];
-    myNewPets.shift();
-    myNewPets.unshift("Lion");
-    myNewPets.pop();
-    return myNewPets;
-}
-console.log(myArrayFunction());
-// End of creating a function
-module.exports = myArrayFunction;
+// Create a temps array here
+var temps = [
+    [31, 32, 19, 37],
+    [29, 27, 55, 36],
+    [17, 27, 42, 46],
+    [29, 52, 21, 64],
+    [91, 27, 31, 61]
+   ];
+   // End of temps array
+   // Create a function below this line
+   function myArrayFunction() {
+       var averageDayTemp = [];
+       for (i = 0; i < 5; i++) {
+           var avrg = 0;
+           for (j = 0; j < 4; j++) {
+               avrg = avrg + temps[i][j];
+           }
+           avrg = Math.round((avrg / 4) * 100) / 100;
+           averageDayTemp.push(avrg);
+       }
+       return averageDayTemp;
+   }
+   console.log(myArrayFunction());
+   // End of creating a function
+   module.exports = myArrayFunction;
